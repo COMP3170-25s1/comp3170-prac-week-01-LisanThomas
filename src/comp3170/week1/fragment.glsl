d2e3ae5c90b93b31fc2a68caa913c88caae87da7
L Thomas <lisan.thomas@students.mq.edu.au>
2025-03-06 15:05:53 +1100

    Updated fragment to create the diamond shape

--- src/comp3170/week1/fragment.glsl
+++ src/comp3170/week1/fragment.glsl
@@ -8,7 +8,7 @@ layout(location = 0) out vec4 o_colour;	// output to colour buffer
 void main() {
    vec2 p = gl_FragCoord.xy / u_screenSize;   // scale p into range (0,0) to (1,1)   
 	vec2 v = abs(p - vec2(0.5));
-	float d = max(v.x, v.y);
+	float d = v.x + v.y;
   
    if (d < 0.3) {
       o_colour = vec4(u_colour, 1);
